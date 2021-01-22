dfd01c373900976341945eaca4fb465fab3a7076
wt123 <s1280120@u-aizu.ac.jp>
2021-01-22 12:21:57 +0900

    Add lec10

--- a10/B.c
+++ a10/B.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
-int p[100][2];
-int maxchain(void);
+int p[100];
+int maxchain(int);
 
 int main(){
     int n,i;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-        scanf("%d%d",&p[i][0],&p[i][1]);
-
-        
+    for(i=1;i<=n;i++)
+        scanf("%d%d",&p[i-1],&p[i]);
+    printf("%d\n",maxchain(n));
+    return 0;
 }
 
 int maxchain(int n){
-    
-    for(int i=0;i<n;i++)
-        
+    int m[100][100],l,k,j,q,i;
+    for(int i=1;i<=n;i++)
+        m[i][i]=0;
+    for(l=2;l<=n;l++){
+        for(i=1;i<=n-l+1;i++){
+            j=i+l-1;
+            m[i][j]=1000000000;
+            for(k=i;k<=j-1;k++){
+                q = m[i][k] + m[k+1][j] + p[i-1] * p[k] * p[j];
+                if(m[i][j]>q) m[i][j] = q;
+            }
+        }
+    }
+        return m[1][n];
 }
